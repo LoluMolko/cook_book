f9d191eeddafeacc77154e9ae6d9a24fecf9e2e8
Marta Kosmala <marta.kos1230@gmail.com>
2018-06-17 19:10:13 +0200

    Add simple form gem

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   validates :title, presence: true
-  validates :difficulty_level, presence: true
+  validates :difficulty_level, presence: true, inclusion: { in: 1..5, message: 'Difficulty level has to be between 1-5' }
   validates :description, presence: true
   has_many :likes
   has_many :users, through: :likes
